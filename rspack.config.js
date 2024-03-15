dd125c635baa897b22f1877187368d0db15099dd
ejfkdev <ejfkdev@users.noreply.github.com>
2024-03-15 15:50:55 +0800

    update

--- rspack.config.js
+++ rspack.config.js
@@ -1,45 +1,56 @@
-const rspack = require('@rspack/core');
-const NodePolyfill = require('@rspack/plugin-node-polyfill');
-const path = require('path');
-const mode = process.env.NODE_ENV ?? 'development'; // production development
-const isDev = process.env.NODE_ENV === 'development';
+// import NodePolyfill from "@rspack/plugin-node-polyfill";
+// import path from 'path';
+const mode = process.env.NODE_ENV ?? "development"; // production development
+// const isDev = process.env.NODE_ENV === "development";
+// import { Configuration } from "@rspack/cli";
+const rspack = require("@rspack/core");
 
-let plugins = [];
-/**
- * @type {import('@rspack/cli').Configuration}
- */
-module.exports = {
+/** @type {import('@rspack/cli').Configuration} */
+const config = {
   experiments: {
+    // outputModule: true,
+    newSplitChunks: true,
     rspackFuture: {
-      // disableTransformByDefault: true,
-      // disableApplyEntryLazily: true,
+      newTreeshaking: true,
+      disableApplyEntryLazily: true,
     },
   },
-  // mode: mode,
-  mode: 'production',
+  optimization: {
+    splitChunks: true,
+    splitChunks: {
+      chunks: "all",
+    },
+    minimize: true,
+    moduleIds: "named",
+    chunkIds: "named",
+  },
+  mode: mode,
+  // mode: "production",
+  target: ["webworker", "es2020"],
   context: __dirname,
   entry: {
-    service_worker: './src/service_worker.ts',
+    service_worker: "./src/service_worker.ts",
   },
   output: {
-    path: path.resolve(__dirname, 'dist/'),
-    chunkFormat: 'module',
+    path: "dist",
+    chunkFilename: "./libs/[name].bundle.js",
+    chunkLoading: "import-scripts",
+    chunkFormat: "array-push",
+    clean: true,
   },
+  externals: {},
   plugins: [
     new rspack.CopyRspackPlugin({
       patterns: [
         {
-          from: './extension',
-          to: '',
+          from: "./extension",
+          to: "",
           force: true,
         },
       ],
     }),
-    new rspack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-    }),
     new rspack.ProgressPlugin({}),
-    new NodePolyfill(),
+    // new NodePolyfill(),
   ].filter(Boolean),
   watch: true,
   watchOptions: {
@@ -50,17 +61,24 @@ module.exports = {
       {
         test: /\.ts$/,
         exclude: [/node_modules/],
-        loader: 'builtin:swc-loader',
+        loader: "builtin:swc-loader",
         options: {
           sourceMap: true,
           jsc: {
             parser: {
-              syntax: 'typescript',
+              syntax: "typescript",
+              preserveAllComments: true,
             },
+            target: "es2020",
           },
         },
-        type: 'javascript/auto',
+        type: "javascript/auto",
+        // env: {
+        //   targets: ['chrome >= 87', 'edge >= 88', 'firefox >= 78'],
+        // },
       },
     ],
   },
 };
+
+module.exports = config;
--- src/service_worker.ts
+++ src/service_worker.ts
@@ -1,117 +1,230 @@
-// @ts-ignore
-import { PNG } from 'pngjs';
-import { jsPDF } from 'jspdf';
+import { imageDimensionsFromData } from "image-dimensions";
+import { jsPDF } from "jspdf";
+import {
+  AsyncPool,
+  Resolve,
+  TaskCallbackArgs,
+  WaitDelayPend,
+  sleep,
+} from "@ejfkdev/async-pool";
+import sortBy from "lodash/sortBy";
 
-const host = 'https://kd.nsfc.cn';
+const host = "https://kd.nsfc.cn";
 
 let downloading = false;
+// 标签页id，用于发送页内通知
+let tabId: number = 0;
 
-const setTask = async (tab?: chrome.tabs.Tab) => {
-  if (tab == undefined || tab.url == undefined) return;
-  const url = new URL(tab.url);
-  const id = url.searchParams.get('id');
-  if (id == null || downloading) return;
-  downloading = true;
-  await chrome.action.setBadgeText({ text: '🩷' });
-  await ReportExport(id);
-  downloading = false;
-  await chrome.action.setBadgeText({ text: '' });
+const init = () => {
+  console.log("onInstalled");
+  chrome.contextMenus.create({
+    type: "normal",
+    id: "@ejfkdev/NsfcReportExportContextMenus",
+    title: "下载结题报告",
+    documentUrlPatterns: ["https://kd.nsfc.cn/finalDetails*"],
+  });
 };
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    type: 'normal',
-    id: '@ejfkdev/NsfcReportExportContextMenus',
-    title: '下载结题报告',
-    documentUrlPatterns: ['https://kd.nsfc.cn/finalDetails*'],
-  });
+chrome.runtime.onInstalled.addListener(init);
 
-  chrome.contextMenus.onClicked.addListener((info, tab) => {
-    setTask(tab);
-  });
-
-  chrome.action.onClicked.addListener(tab => {
-    setTask(tab);
-  });
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  switch (info.menuItemId) {
+    case "@ejfkdev/NsfcReportExportContextMenus":
+      setTask(tab);
+  }
 });
 
-const fetchReportImage = async (id: string, index: number) => {
-  try {
-    const data = await fetch(`${host}/api/baseQuery/completeProjectReport`, {
-      headers: {
-        accept: 'application/json, text/plain, */*',
-        'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-        'content-type': 'application/x-www-form-urlencoded',
-      },
-      body: `id=${id}&index=${index}`,
-      method: 'POST',
-    }).then(response => response.json());
-    const url = host + data.data.url;
-    // 获取图片内容
-    const blob = await fetch(url, {
-      headers: {
-        accept: 'application/json, text/plain, */*',
-        'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-        'content-type': 'application/x-www-form-urlencoded',
-      },
-      method: 'GET',
-    })
-      .then(response => {
-        if (response.ok) return response;
-        else throw new Error(`HTTP error! status: ${response.status}`);
-      })
-      .then(response => response.blob());
-    const img = await blob.arrayBuffer();
-    return img;
-  } catch (error) {
-    console.log(error);
-  }
-  return null;
+chrome.action.onClicked.addListener((tab) => {
+  setTask(tab);
+});
+
+// 网页内进度通知
+const pageToast = () => {
+  const callback = (data: ToastMsg) => {
+    let root = document.querySelector(
+      `div#NsfcReportExportToastRoot`
+    ) as HTMLDivElement;
+    let title = document.querySelector(
+      `div#NsfcReportExportToastTitle`
+    ) as HTMLDivElement;
+    let message = document.querySelector(
+      `div#NsfcReportExportToastMessage`
+    ) as HTMLDivElement;
+    if (document.querySelector(`div#NsfcReportExportToastRoot`) == null) {
+      root = document.createElement("div");
+      root.id = `NsfcReportExportToastRoot`;
+      root.style.cssText = `
+      top: 80%;
+      left: 0;
+      right: 0;
+      margin: 0 auto;
+      padding: 0.5rem;
+      position: fixed;
+      display: flex;
+      flex-direction: column;
+      width: 20rem;
+      outline: 0.25rem solid rgb(0 128 255 / 30%);
+      border-radius: 1.25rem;
+      background-color: rgb(255 255 255 / 50%);
+      backdrop-filter: blur(1rem);`;
+      title = document.createElement("div");
+      title.id = `NsfcReportExportToastTitle`;
+      title.style.cssText = `font-size:1.25rem; font-weight:bold; color:#444;`;
+      message = document.createElement("div");
+      message.id = `NsfcReportExportToastMessage`;
+      message.style.cssText = `color:#666; text-wrap: pretty; word-break: break-word;`;
+      root.appendChild(title);
+      root.appendChild(message);
+      document.body.appendChild(root);
+    } else {
+      //@ts-ignore
+      clearInterval(window["NsfcReportExportToastRootSetTimeout"]);
+    }
+
+    title.innerText = data.title;
+    message.innerText = data.message;
+
+    if (data.type === "done") {
+      // chrome.runtime.onMessage.removeListener(callback);
+      //@ts-ignore
+      window["NsfcReportExportToastRootSetTimeout"] = setTimeout(
+        () => document.body.removeChild(root),
+        10000
+      );
+    }
+    return undefined;
+  };
+  chrome.runtime.onMessage.addListener(callback);
 };
 
+// 启动下载任务
+const setTask = async (tab?: chrome.tabs.Tab) => {
+  if (tab == undefined || tab.url == undefined || tab.id == undefined) return;
+  const url = new URL(tab.url);
+  const id = url.searchParams.get("id");
+  if (id == null || downloading) return;
+  downloading = true;
+  console.log("setTask", id);
+  tabId = tab.id;
+  chrome.scripting.executeScript({
+    target: { tabId },
+    func: pageToast,
+  });
+  await chrome.action.setBadgeText({ text: "⏬" });
+  await ReportExport(id);
+  downloading = false;
+  tabId = 0;
+  await chrome.action.setBadgeText({ text: "" });
+};
+
+/**
+ * 获取一页报告图片
+ * @param id 报告id
+ * @param index 第几页
+ * @returns
+ */
+const fetchTask = async ([id, index]: [string, number]) => {
+  const data = await fetch(`${host}/api/baseQuery/completeProjectReport`, {
+    headers: {
+      accept: "application/json, text/plain, */*",
+      "accept-language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+      "content-type": "application/x-www-form-urlencoded",
+    },
+    body: `id=${id}&index=${index}`,
+    method: "POST",
+  }).then((response) => response.json());
+  const imgURL = host + data.data.url;
+  console.log(id, index, imgURL);
+  // 获取图片内容
+  const blob = await fetch(imgURL, {
+    headers: {
+      accept: "application/json, text/plain, */*",
+      "accept-language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+      "content-type": "application/x-www-form-urlencoded",
+    },
+    method: "GET",
+  })
+    .then((response) => {
+      if (response.ok) return response;
+      else {
+        const err = new Error(`HTTP error! status: ${response.status}`);
+        err.name = response.status.toString();
+        throw err;
+      }
+    })
+    .then((response) => response.blob());
+  const imgArrayBuffer = new Uint8Array(await blob.arrayBuffer());
+  // 从图片内容解析宽高
+  const size = imageDimensionsFromData(imgArrayBuffer);
+  return {
+    index,
+    imageData: imgArrayBuffer,
+    width: size?.width,
+    height: size?.height,
+    // pdf竖版或横版页
+    orientation: (size?.width ?? 0) < (size?.height ?? 0) ? "p" : "l",
+  };
+};
+
+/**
+ * 获取报告名称等基础信息
+ * @param id
+ * @returns
+ */
 const getProjectInfo = async (id: string) => {
+  console.log("getProjectInfo", id);
   const response = await fetch(
     `${host}/api/baseQuery/conclusionProjectInfo/${id}`,
     {
       headers: {
-        accept: 'application/json, text/plain, */*',
-        'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-        'content-type': 'application/x-www-form-urlencoded',
+        accept: "application/json, text/plain, */*",
+        "accept-language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+        "content-type": "application/x-www-form-urlencoded",
       },
       body: null,
-      method: 'POST',
-      mode: 'cors',
-    },
+      method: "POST",
+      mode: "cors",
+    }
   );
   if (response.ok) {
     const data = await response.json();
     return {
-      projectName: data?.data?.projectName ?? 'projectName',
+      projectName: data?.data?.projectName ?? "projectName",
       projectAdmin:
-        (data?.data?.projectAdmin ?? '') + ' ' + (data?.data?.dependUnit ?? ''),
-      conclusionAbstract: data?.data?.conclusionAbstract ?? '',
-      projectKeywordC: data?.data?.projectKeywordC ?? '',
+        (data?.data?.projectAdmin ?? "") + " " + (data?.data?.dependUnit ?? ""),
+      conclusionAbstract: data?.data?.conclusionAbstract ?? "",
+      projectKeywordC: data?.data?.projectKeywordC ?? "",
     };
   } else {
     return {
-      projectName: 'projectName',
-      projectAdmin: '',
-      conclusionAbstract: '',
-      projectKeywordC: '',
+      projectName: "projectName",
+      projectAdmin: "",
+      conclusionAbstract: "",
+      projectKeywordC: "",
     };
   }
 };
 
+/**
+ * 获取完整报告并保存pdf
+ * @param id
+ */
 const ReportExport = async (id: string) => {
+  console.log("ReportExport", id);
+  const startTime = Date.now();
   const info = await getProjectInfo(id);
-  const notiId = await chrome.notifications.create('ReportExportStart', {
-    type: 'basic',
-    iconUrl: 'images/logo.png',
-    title: '正在导出',
+  const notiId = await chrome.notifications.create("ReportExportStart", {
+    type: "basic",
+    iconUrl: "images/logo.png",
+    title: "正在导出",
     message: info.projectName,
     silent: true,
   });
-  let index = 0;
+  chrome.tabs.sendMessage(tabId, {
+    title: "正在导出",
+    message: info.projectName,
+  });
+
   const doc = new jsPDF({
     compress: true,
   });
@@ -120,55 +233,161 @@ const ReportExport = async (id: string) => {
     subject: info.conclusionAbstract,
     author: info.projectAdmin,
     keywords: info.projectKeywordC,
-    creator: '@ejfkdev',
+    creator: "@ejfkdev",
   });
   doc.deletePage(1);
+  let imgFetchDone = false;
+  let imgCache: ImgResultType[] = [];
+  const [pend, done, cancel] = WaitDelayPend(5000, 1000 * 60 * 60);
+  // 页数递增获取图片，遇到404即为最后一页
+  const imgPool = new AsyncPool<[string, number]>({
+    name: "获取图片并插入PDF",
+    parallel: 4,
+    maxRetryCount: 5,
+    worker: fetchTask,
+    rateLimiter: async (done: Resolve) => {
+      await sleep(1000);
+      done();
+    },
+    allWorkerDoneCallback: () => {
+      done();
+    },
+    taskErrorCallback: (args: TaskCallbackArgs<[string, number]>) => {
+      console.log(args.error);
+      if (!args.error.message.includes("404")) {
+        cancel();
+        args.retry();
+      } else {
+        imgFetchDone = true;
+      }
+    },
+    taskResultCallback: (args: TaskCallbackArgs<[string, number]>) => {
+      cancel();
+      if (args.error != null) return;
+      console.log("图片下载完成", args.data);
+      imgCache.push(args.result as ImgResultType);
+    },
+  });
+
+  let index = 0;
   while (++index) {
+    if (imgFetchDone) break;
+    await imgPool.waitParallelIdel();
+    console.log(
+      "ReportExport",
+      id,
+      "正在导出",
+      `${info.projectName} 第${index}页`
+    );
     await chrome.notifications.update(notiId, {
-      type: 'basic',
-      iconUrl: 'images/logo.png',
-      title: '正在导出',
+      type: "basic",
+      iconUrl: "images/logo.png",
+      title: "正在导出",
       message: `${info.projectName} 第${index}页`,
       silent: true,
     });
-    const img = await fetchReportImage(id, index);
-    if (img == null) break;
-    const png = new PNG().parse(img as Buffer);
-    doc.addPage([png.width, png.height], png.width < png.height ? 'p' : 'l');
-    doc.addImage(new Uint8Array(img), 'PNG', 0, 0, png.width, png.height);
-    await new Promise(resolve => setTimeout(resolve, 1000));
+    chrome.tabs.sendMessage(tabId, {
+      title: "正在导出",
+      message: `${info.projectName} 第${index}页`,
+    });
+    cancel();
+    imgPool.addTodo([id, index]);
+    await sleep(1000);
   }
-  const pdfUrl = doc.output('datauristring');
+
+  await sleep(5000);
+  await pend();
+
+  console.log("生成中", imgCache.length);
+  await chrome.notifications.update(notiId, {
+    type: "basic",
+    iconUrl: "images/logo.png",
+    title: "正在导出",
+    message: `${info.projectName}.pdf 生成中`,
+    silent: true,
+  });
+  chrome.tabs.sendMessage(tabId, {
+    title: "正在导出",
+    message: `${info.projectName}.pdf 生成中`,
+  });
+
+  // 图片下载可能乱序，保存前按页码排序
+  imgCache = sortBy(imgCache, "index");
+
+  for (const data of imgCache) {
+    doc.addPage([data.width, data.height], data.orientation);
+    doc.addImage({
+      imageData: data.imageData,
+      format: "PNG",
+      x: 0,
+      y: 0,
+      width: data.width,
+      height: data.height,
+      compression: "FAST",
+    });
+  }
+
+  const pdfUrl = doc.output("datauristring");
   const filename = `${info.projectName}.pdf`;
   chrome.downloads.download(
     {
       url: pdfUrl,
       filename: filename,
-      conflictAction: 'uniquify',
+      conflictAction: "uniquify",
       saveAs: false,
     },
     function (downloadId) {
       console.log(`文件下载，ID: ${downloadId}`);
-    },
+    }
   );
-  await chrome.notifications.create('ReportExportStart', {
-    type: 'basic',
-    iconUrl: 'images/logo.png',
-    title: '导出完成',
-    message: filename,
-    silent: true,
+  let costTime = (Date.now() - startTime) / 1000 / 60;
+  costTime = Math.ceil(costTime);
+  console.log(
+    "ReportExport",
+    id,
+    "导出完成",
+    filename,
+    `耗时${costTime}分钟`,
+    `共${imgCache.length}页`
+  );
+  await chrome.notifications.create("ReportExportStart", {
+    type: "basic",
+    iconUrl: "images/logo.png",
+    title: "导出完成",
+    message: `${filename} 共${imgCache.length}页 用时${costTime}分钟`,
+    silent: false,
+  });
+  chrome.tabs.sendMessage(tabId, {
+    type: "done",
+    title: "导出完成",
+    message: `${filename} 共${imgCache.length}页 用时${costTime}分钟`,
   });
 };
 
 const onMessage = (data: any, sender: chrome.runtime.MessageSender) => {
   if (!Array.isArray(data) || data.length != 2) return;
-  console.log('sw.js onMessage get message:', data);
-  console.log('sw.js onMessage get sender:', sender);
+  console.log("sw.js onMessage get message:", data);
+  console.log("sw.js onMessage get sender:", sender);
   ReportExport(data[0]);
   return true;
 };
 
 // https://kd.nsfc.cn/finalDetails?id=18762a8003cdc2a63d65957925f6c67d
 // ReportExport('18762a8003cdc2a63d65957925f6c67d');
+// https://kd.nsfc.cn/finalDetails?id=161cd5723f90c0d39423fb838e32f809
 
 chrome.runtime.onMessageExternal.addListener(onMessage);
+
+type ImgResultType = {
+  index: string;
+  imageData: Uint8Array;
+  width: any;
+  height: any;
+  orientation: "p" | "l" | "portrait" | "landscape" | undefined;
+};
+
+type ToastMsg = {
+  type?: "show" | "done";
+  title: string;
+  message: string;
+};
